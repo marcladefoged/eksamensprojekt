c385bcae96343fe2ab780b67b594438785141e4d
marcladefoged <marcladefogedjensen@gmail.com>
2021-01-12 22:02:38 +0100

    burgermenu added to frontpage

---
+++ script.js
@@ -0,0 +1,7 @@
+function onClickMenu() {
+    console.log("klik");
+
+    document.getElementById("menu").classList.toggle("change");
+
+    document.getElementById("nav").classList.toggle("change");
+}
